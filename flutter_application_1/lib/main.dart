5da6b2b9f9ba0f65fc231be4d9510966cfc46e66
lleryT <tljluv22@gmail.com>
2024-09-19 03:29:23 -0400

    Test Push

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 4, 255)),
         ),
         home: MyHomePage(),
       ),

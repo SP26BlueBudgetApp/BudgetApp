e4b963b22c6bd575b06c05d74ce6e7b4e90b391f
lleryT <tljluv22@gmail.com>
2024-11-03 17:28:56 -0500

    Created Login and Signup
    Login and signup screen created with basic functionality like switching pages. No database built in yet though.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -6,72 +6,290 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
+  _MyAppState createState() => _MyAppState();
+  }
+
+class _MyAppState extends State<MyApp> { //Login Page
+  @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 0, 4, 255)),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Login Screen'),
         ),
-        home: MyHomePage(),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 30
+              ),
+              child: SizedBox( //Logo
+                height: 250,
+                child: Image.asset(
+                  "images/pngtree-bank-icon-png-image_1757496.jpg",
+                  fit: BoxFit.contain
+                ),
+              ),
+            ),
+
+            Padding( //Wrapping in padding adds gap
+              padding: const EdgeInsets.symmetric(
+                vertical: 30,
+                horizontal: 16
+              ),
+              child: Form( //Email
+                child: Column(
+                  children: [
+              
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter Email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {
+              
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'Please input an email' : null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+             Padding( //Wrapping in padding adds gap
+               padding: const EdgeInsets.symmetric(
+                horizontal: 16
+              ),
+               child: Form( //Password
+                child: Column(
+                  children: [
+               
+                    TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter password',
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {
+               
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'Please input a password' : null;
+                      },
+                    ),
+               
+                    SizedBox(height: 10),
+                    Builder(
+                      builder: (context) {
+                        return Padding( //Login Button
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: MaterialButton(
+                            minWidth: double.infinity,
+                            onPressed: () {
+                              Navigator.of(context)
+                              .push(
+                                MaterialPageRoute(
+                                  builder: (context) => signupPage()
+                                  ),
+                              );
+                            },
+                            child: Text('Login'),
+                            color: const Color.fromARGB(255, 83, 83, 83),
+                            textColor:Colors.white,
+                          ),
+                        );
+                      }
+                    ),
+                    Builder(
+                      builder: (context) {
+                        return Padding( //SignUp button
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 160,
+                            vertical: 20
+                          ),
+                          child: MaterialButton(
+                            minWidth: double.infinity,
+                            onPressed: () {
+                              Navigator.of(context)
+                              .push(
+                                MaterialPageRoute(
+                                  builder: (context) => signupPage()
+                                ),
+                              );
+                            },
+                            child: Text('Signup'),
+                            color: const Color.fromARGB(255, 83, 83, 83),
+                            textColor:Colors.white,
+                          ),
+                        );
+                      }
+                    ),
+                  ],
+                ),
+              ),
+             ),
+          ],
+        ),
       ),
     );
   }
 }
 
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
+/// Add the second screen
+class signupPage extends StatefulWidget { //SignUp Page
+  const signupPage({Key? key}) : super(key: key);
 
-  void getNext() {
-    current = WordPair.random();
-    notifyListeners();
-  }
+  @override
+  _signupState createState() => _signupState();
 }
 
-class MyHomePage extends StatelessWidget {
+class _signupState extends State<signupPage> {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    return Scaffold(
+    return MaterialApp(
+      home: Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Sign-Up Page"),
+      ),
       body: Column(
-        children: [
-          Text('A random AWESOME COOL idea:'),
-          BigCard(pair: pair),
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 30
+              ),
+              child: SizedBox( //Logo
+                height: 150,
+                child: Image.asset(
+                  "images/pngtree-bank-icon-png-image_1757496.jpg",
+                  fit: BoxFit.contain
+                ),
+              ),
+            ),
 
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
-      ),
-    );
-  }
-}
+          Padding( //Wrapping in padding adds gap
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16
+              ),
+              child: Form( //Email Signup
+                child: Column(
+                  children: [
+              
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Enter Email',
+                        prefixIcon: Icon(Icons.email),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {
+              
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'Please input an email' : null;
+                      },
+                    ),
+                  ],
+                ),
+              ),
+            ),
 
-class BigCard extends StatelessWidget {
-  const BigCard({
-    super.key,
-    required this.pair,
-  });
+          Padding( //Wrapping in padding adds gap
+              padding: const EdgeInsets.symmetric(
+                vertical: 30,
+                horizontal: 16
+              ),
+              child: Form( //Email Signup
+                child: Column(
+                  children: [
+              
+                    TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Enter Password',
+                        prefixIcon: Icon(Icons.password),
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (String value) {
+              
+                      },
+                      validator: (value) {
+                        return value!.isEmpty ? 'Please input an password' : null;
+                      },
+                    ),
+                  
+                    SizedBox(height: 14),//Gap
 
-  final WordPair pair;
+                    Builder( //SignUp button
+                      builder: (context) {
+                        return Padding( 
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: MaterialButton(
+                            minWidth: double.infinity,
+                            onPressed: () {
+                              Navigator.of(context)
+                              .push(
+                                MaterialPageRoute(
+                                  builder: (context) => MyApp()
+                                ),
+                              );
+                            },
+                            color: const Color.fromARGB(255, 83, 83, 83),
+                            textColor:Colors.white,
+                            child: Text('Signup'),
+                          ),
+                        );
+                      }
+                    ),
 
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(pair.asLowerCase),
+                    Builder( // Back to login
+                      builder: (context) {
+                        return Padding( 
+                          padding: const EdgeInsets.symmetric(horizontal: 40),
+                          child: TextButton(
+                            onPressed: () {
+                              Navigator.of(context)
+                              .push(
+                                MaterialPageRoute(
+                                  builder: (context) => MyApp()
+                                ),
+                              );
+                            },
+                            style: TextButton.styleFrom(
+                              foregroundColor: const Color.fromARGB(255, 99, 99, 99),
+                            ),
+                            child: Text('Back to login'),
+                          ),
+                        );
+                      }
+                    ),
+
+                  ],
+                ),
+              ),
+            ),
+          
+          ]
+        ),
       ),
     );
   }

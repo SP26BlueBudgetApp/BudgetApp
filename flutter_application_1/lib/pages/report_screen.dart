3ed7c065aaad76989ae85a9d29611a561786d6e6
lleryT <tljluv22@gmail.com>
2024-11-12 18:52:50 -0500

    Pie Chart
    Integrated a pie chart taking in the savings/income/expense from the database and creating a piece for each one with a percentage and icon

--- flutter_application_1/lib/pages/report_screen.dart
+++ flutter_application_1/lib/pages/report_screen.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter_application_1/auth.dart';
 import 'package:flutter_application_1/pages/signup_screen.dart';
 import 'package:flutter_application_1/pages/login_screen.dart';
@@ -11,6 +12,7 @@ import 'package:flutter_application_1/services/graphDatabase_service.dart';
 import 'package:flutter_application_1/models/graph.dart';
 import 'package:flutter_application_1/widgets/textfield.dart';
 import 'package:flutter_application_1/widgets/buttons.dart';
+import 'package:flutter_application_1/widgets/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -24,6 +26,8 @@ class ReportScreen extends StatefulWidget {
 class _ReportScreenState extends State<ReportScreen> { //Budget page
   final TextEditingController _textEditingController1 = TextEditingController();
   final TextEditingController _textEditingController2 = TextEditingController();
+
+  int touchedIndex=0;
 
   final GraphDatabaseService _databaseService = GraphDatabaseService();
   @override
@@ -70,28 +74,11 @@ class _ReportScreenState extends State<ReportScreen> { //Budget page
               String reportID = graph[index].id;
             return Padding(
               padding: const EdgeInsets.symmetric(
-                vertical: 10,
+                vertical: 55,
                 horizontal: 10,
               ),
               child: Column(
                 children: [
-                  TextButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      foregroundColor: WidgetStatePropertyAll(Colors.black),
-                      shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),                        )
-                        ),
-                    ),
-                    child: Text(
-                        "Expense: " + "\$" + report.Expense.toString(),
-                        style: TextStyle(fontSize: 32),
-                      ),
-                    onLongPress: () {
-                      _displayTextInputDialogExpense(reportID);
-                    },  
-                  ),
                   TextButton(
                     onPressed: () {},
                     style: ButtonStyle(
@@ -109,6 +96,23 @@ class _ReportScreenState extends State<ReportScreen> { //Budget page
                       _displayTextInputDialogExpense(reportID);
                     },  
                   ),
+                  TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      foregroundColor: WidgetStatePropertyAll(Colors.black),
+                      shape: WidgetStatePropertyAll<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20),                        )
+                        ),
+                    ),
+                    child: Text(
+                        "Expense: " + "\$" + report.Expense.toString(),
+                        style: TextStyle(fontSize: 32),
+                      ),
+                    onLongPress: () {
+                      _displayTextInputDialogExpense(reportID);
+                    },  
+                  ),
                   SizedBox(height: 20),
                   Text(
                     "Savings: " + "\$" + report.Saving.toString(),
@@ -117,6 +121,31 @@ class _ReportScreenState extends State<ReportScreen> { //Budget page
                           color: Colors.black,
                           fontWeight: FontWeight.bold,
                           ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 50.0),
+                    child: SizedBox(
+                      height: 350,
+                      child: PieChart(
+                        PieChartData(
+                          pieTouchData: PieTouchData(
+                            touchCallback: 
+                              (FlTouchEvent event, pieTouchResponse) {
+                                setState(() {
+                                  if (!event.isInterestedForInteractions || pieTouchResponse == null || pieTouchResponse.touchedSection == null) {
+                                    touchedIndex = -1;
+                                  return;
+                                }
+                                  touchedIndex = pieTouchResponse.touchedSection!.touchedSectionIndex;
+                                });
+                              }
+                          ),
+                          sections: pieChartSection(report.Expense,report.Income,report.Saving),
+                          sectionsSpace: 3,
+                          centerSpaceRadius: 0,
+                        ),
+                      ),
+                    ),
                   ),
                 ],             
             )
@@ -136,11 +165,11 @@ class _ReportScreenState extends State<ReportScreen> { //Budget page
             children: [
               TextField(
                 controller: _textEditingController1, 
-                decoration: InputDecoration(hintText: "Expense: ex. 100"),
+                decoration: InputDecoration(hintText: "Income: ex. 100"),
               ),
               TextField(
                 controller: _textEditingController2, 
-                decoration: InputDecoration(hintText: "Income: ex. 100"),
+                decoration: InputDecoration(hintText: "Expense: ex. 100"),
               ),
             ],
           ),
@@ -151,8 +180,8 @@ class _ReportScreenState extends State<ReportScreen> { //Budget page
               child: Text('Change'),
               onPressed: () {
                 Graph report = Graph(
-                  Expense: int.parse(_textEditingController1.text),
-                  Income: int.parse(_textEditingController2.text), 
+                  Expense: int.parse(_textEditingController2.text),
+                  Income: int.parse(_textEditingController1.text), 
                   Saving: int.parse(_textEditingController1.text) - int.parse(_textEditingController2.text), 
                 );
                 _databaseService.updateGraph(reportID, report);
@@ -164,6 +193,94 @@ class _ReportScreenState extends State<ReportScreen> { //Budget page
           ]
         );
       }
+    );
+  }
+
+  List<PieChartSectionData> pieChartSection(int Expense, int Income, int Savings) {
+
+    int total = Expense+Income+Savings;
+    double expensePercent = Expense / total * 100;
+    double incomePercent = Income / total * 100;
+    double savingsPercent = Savings / total * 100;
+
+    List<Color> sectionColors = [
+      Colors.red,
+      Colors.green,
+      Colors.orange,
+    ];
+
+    List<IconData> sectionIcons = [
+      Icons.arrow_downward,
+      Icons.arrow_upward,
+      Icons.savings,
+    ];
+
+    return List.generate(
+      3, 
+      (index) {
+        final isTouched = index == touchedIndex;
+        final radius = isTouched ? 190.0 : 150.0;
+        final fontSize = isTouched ? 30.0 : 24.0;
+        final widgetSize = isTouched ? 50.0 : 40.0;
+        switch (index) {
+          case 0:
+            return PieChartSectionData(
+              color: sectionColors[index],
+              value: expensePercent,
+              title: expensePercent.toString() + '%',
+              radius: radius,
+              titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              badgeWidget: CustomBadge(
+                icon: sectionIcons[index], 
+                size: widgetSize, 
+                borderColor: Colors.black
+                ),
+              badgePositionPercentageOffset: 0.98,
+            );
+          case 1:
+            return PieChartSectionData(
+              color: sectionColors[index],
+              value: incomePercent,
+              title: incomePercent.toString() + '%',
+              radius: radius,
+              titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              badgeWidget: CustomBadge(
+                icon: sectionIcons[index], 
+                size: widgetSize, 
+                borderColor: Colors.black
+                ),
+              badgePositionPercentageOffset: 0.98,
+            );
+          case 2:
+            return PieChartSectionData(
+              color: sectionColors[index],
+              value: savingsPercent,
+              title: savingsPercent.toString() + '%',
+              radius: radius,
+              titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              badgeWidget: CustomBadge(
+                icon: sectionIcons[index], 
+                size: widgetSize, 
+                borderColor: Colors.black
+                ),
+              badgePositionPercentageOffset: 0.98,
+            );
+        }
+        return PieChartSectionData(
+        );
+      },
     );
   }
 }
